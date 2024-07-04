5d02459d98747d3ce3510842dd5b1a4dcab0662b
efa07 <efatariku07@gmail.com>
2024-07-04 20:33:33 +0300

    file created

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""Type-annotated function concat"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """Takes two str args and returns their concatenation as str"""
+    return str1 + str2
